7e5d98631ab2d237cfece663ef61c8bda53c8b93
Craig <972350+craigls@users.noreply.github.com>
2021-07-02 00:53:07 -0300

    Removed unused file

--- loader.js
+++
@@ -1,9 +0,0 @@
-
-function loadRomFromFile(file) {
-  let reader = new FileReader();
-  reader.readAsArrayBuffer(file);
-  reader.onload = function() {
-    dmg.loadRom(Array.from(new Uint8Array(reader.result)));
-    dmg.start();
-  }
-}
